f135ecbe2eaaeb777a9e8b8094e74674133225ee
alexzms <3036648523@qq.com>
2026-03-12 02:29:55 +0000

    dataset downloading script

---
+++ examples/train/download_dataset/ltx2_dataset.sh
@@ -0,0 +1,4 @@
+python scripts/huggingface/download_hf.py --repo_id "wlsaidhi/crush-smol-merged" --local_dir "data/ltx2_training_data_crush_smol" --repo_type "dataset"
+
+python scripts/huggingface/download_hf.py --repo_id "Davids048/ltx2-data" --local_dir "data/ltx2_training_data_david" --repo_type "dataset"
+
